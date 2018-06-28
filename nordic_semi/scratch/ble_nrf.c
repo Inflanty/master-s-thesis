5dbf7ddc5707939aabe959c5206b214a56f35e05
Inflanty <inflanty@gmail.com>
2018-06-28 16:53:38 +0200

    master : start scan

--- nordic_semi/scratch/ble_nrf.c
+++ nordic_semi/scratch/ble_nrf.c
@@ -6,19 +6,25 @@
 */
 error_type_t searchForDevice ( device_type_t device )
 {
+  uint32_t error_code = 0;
   switch( device )
   {
     case THINGY :
       ble_gap_addr_t deviceAddress = { BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE, {0x71,0xA2,0xEC,0xDA,0x5F,0xE0}};
+      
     break;
     case SENSOR :
       ble_gap_addr_t deviceAddress = { BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE, {0x71,0xA2,0xEC,0xDA,0x5F,0xE1}};
+      
     break;
     case TEST_DEVICE :
       ble_gap_addr_t deviceAddress = { BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE, {0x71,0xA2,0xEC,0xDA,0x5F,0xFF}};
+      
     break;
     default:
 
     break;
   }
+  pAddWhitelist[0] = &deviceAddress;
+  sd_ble_gap_whitelist_set(pAddWhitelist , 1);
 }
--- nordic_semi/scratch/ble_nrf.h
+++ nordic_semi/scratch/ble_nrf.h
@@ -8,7 +8,17 @@
 #define SCAN_TIMEOUT 0xFFFF //0x0000 - 0xFFFF
 #define SCAN_WINDOW 500
 
-ble_gap_scan_params_t m_scan_param = { .active = 1, .interval = SCAN_INTERVAL, .whitelist = NULL, .timeout = SCAN_TIMEOUT, .window = SCAN_WINDOW, };
+ble_gap_scan_params_t m_scan_param = {  .active = 1, 
+                                        .channel_mask = 0, 
+                                        .extended = 0, 
+                                        .filter_policy = BLE_GAP_SCAN_FP_WHITELIST, 
+                                        .interval = SCAN_INTERVAL, 
+                                        .report_incomplete_evt = 0, 
+                                        .scan_phys = BLE_GAP_PHY_AUTO, 
+                                        .timeout = SCAN_TIMEOUT, 
+                                        .window = SCAN_WINDOW,  };
+
+ble_gap_addr_t                          pAddWhitelist[1];
 
 typedef enum {
   OK,
